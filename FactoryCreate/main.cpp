a9f479f0f2a0489a4777924fc2405eb57c001188
faststone <395459853@qq.com>
2017-12-08 11:33:25 +0800

    1、增加Test

--- FactoryCreate/main.cpp
+++ FactoryCreate/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    Factory * factory = Factory::instance();
+    Data * p1 = factory->create("Data1");
+    p1->test();
+
+    Data * p2 = factory->create("Data2");
+    p2->test();
     cout << "Hello World!" << endl;
     return 0;
 }
